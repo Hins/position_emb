ce8e338dea3a6c05cfc90c05b647ab0003d57d17
panxiaotong <panxiaotong@xiaomi.com>
2018-08-28 11:20:33 +0800

    support english language in word break

--- util/word_break.py
+++ util/word_break.py
@@ -8,8 +8,8 @@ import sys
 import jieba
 
 if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        print("word_break <input file> <stopword file> <entity file> <output_file>")
+    if len(sys.argv) < 6:
+        print("word_break <input file> <stopword file> <entity file> <language> <output_file>")
         exit()
 
     jieba.load_userdict(sys.argv[3])
@@ -23,14 +23,19 @@ if __name__ == "__main__":
     integ_digit_pattern = re.compile(r"-?[1-9]\d*")
     #english_char_pattern = re.compile(r"[0-9a-zA-Z.]+")
 
-    with open(sys.argv[4], 'w') as out_f:
+    with open(sys.argv[5], 'w') as out_f:
         with open(sys.argv[1], 'r') as in_f:
             for line in in_f:
-                seg_list = jieba.cut(line.strip('\r\n').lower(), cut_all=False)
-                seg_list = [item for item in seg_list if item.encode('utf-8') not in stopword_dict and item.strip() != "" and
-                            float_digit_pattern.match(item.encode("utf-8")) == None and
-                            integ_digit_pattern.match(item.encode("utf-8")) == None]
-                            #and english_char_pattern.match(item.encode("utf-8")) == None]
-                out_f.write(",".join(seg_list).encode('utf-8') + "\n")
+                if sys.argv[4].lower() == "chinese":
+                    seg_list = jieba.cut(line.strip('\r\n').lower(), cut_all=False)
+                    seg_list = [item for item in seg_list if item.encode('utf-8') not in stopword_dict and item.strip() != "" and
+                                float_digit_pattern.match(item.encode("utf-8")) == None and
+                                integ_digit_pattern.match(item.encode("utf-8")) == None]
+                                #and english_char_pattern.match(item.encode("utf-8")) == None]
+                    out_f.write(",".join(seg_list).encode('utf-8') + "\n")
+                elif sys.argv[4].lower() == "english":
+                    elements = [item for item in line.split(" ") if item.strip() != " " and
+                                item not in stopword_dict]
+                    out_f.write(",".join(elements) + "\n")
             in_f.close()
         out_f.close()
